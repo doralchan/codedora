1ff71b6e505ec691595c1c9d8c597d8e19960d97
Dora Chan <dora.l.chan@gmail.com>
2017-11-04 18:10:24 -0700

    clean out

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Home from './projects/home/index.jsx';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Home />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/projects/home/index.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './styles.css';
+
+class Home extends Component {
+
+  render() {
+    return (
+      <div idName='home'>
+        okie doke
+      </div>
+    );
+  }
+}
+
+export default Home
