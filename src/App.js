3208ecf8ec77e4ee1822dc57337d3d4880d17e2b
Dora Chan <dora.l.chan@gmail.com>
2019-10-02 16:38:17 -0700

    testing branch

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
 import React, { Component } from 'react';
 import { BrowserRouter } from 'react-router-dom';
 
-import Main from './components/main';
-
-import Hero from './components/_hero';
-import Skills from './components/_skills';
-import Work from './components/_work';
-import Blog from './components/_blog';
-import Contact from './components/_contact';
-
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
-        <Main>
-          <Hero />
-          <Skills />
-          <Work />
-          <Blog />
-          <Contact />
-        </Main>
+        <div></div>
       </BrowserRouter>
     );
   }
